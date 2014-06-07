2e3ffa048d1f6bbc4896cf02cd65e5ba74cac369
Ievgen Varavva <fuzzy.parabola@gmail.com>
2014-06-07 16:15:17 +0100

    More godocs.

--- commands.go
+++ commands.go
@@ -1,4 +1,22 @@
-// Package roomba iRobot roomba open interface.
+// Package roomba iRobot roomba Open Interface.
+//
+// The Roomba OI has four operating modes: Off, Passive, Safe, and Full.
+// When roomba starts the OI is in “off” mode. When it is off, the OI listens
+// for an OI Start command. Once it receives the Start command, you can enter
+// into any one of the four operating modes by sending a mode command to the OI.
+//
+// Passive mode: entered upon sending one of the cleaning commands. You can
+// only read sensor data in the passive mode and can't change the actuators
+// state.
+//
+// Safe mode: gives full control of Roomba, except for safety restrictions:
+//   * Cliff detection when moving forward.
+//	 * Detection of wheel drop.
+// 	 * Charger plugged in and powered.
+// When any of the events ocurs, Roomba switches to passive mode.
+//
+// Full mode: gives full control over Romoba, disabling the safety
+// restrictions.
 
 package roomba
 
@@ -24,6 +42,8 @@ func to_byte(b bool) byte {
 
 var OpCodes = constants.OpCodes
 
+// MakeRoomba initializes a new Roomba structure and sets up a serial port.
+// By default, Roomba communicates at 115200 baud.
 func MakeRoomba(port_name string) (*Roomba, error) {
 	roomba := &Roomba{PortName: port_name, StreamPaused: make(chan bool, 1)}
 	baud := uint(115200)
@@ -31,44 +51,76 @@ func MakeRoomba(port_name string) (*Roomba, error) {
 	return roomba, err
 }
 
+// Start command starts the OI. You must always send the Start command before
+// sending any other commands to the OI.
 // Note: Use the Start command (128) to change the mode to Passive.
 func (this *Roomba) Start() error {
 	return this.WriteByte(OpCodes["Start"])
 }
 
+// TODO: Baud command.
+
+// Passive switches Roomba to passive mode by sending the Start command.
 func (this *Roomba) Passive() error {
 	return this.Start()
 }
 
+// This command puts the OI into Safe mode, enabling user control of Roomba.
+// It turns off all LEDs.
 func (this *Roomba) Safe() error {
 	return this.WriteByte(OpCodes["Safe"])
 }
 
+// Full command gives you complete control over Roomba by putting the OI into
+// Full mode, and turning off the cliff, wheel-drop and internal charger safety
+// features.
 func (this *Roomba) Full() error {
 	return this.WriteByte(OpCodes["Full"])
 }
 
+// Control command's effect and usage are identical to the Safe command.
 func (this *Roomba) Control() error {
 	this.Passive()
 	return this.WriteByte(130) // ?
 }
 
+// Clean command starts the default cleaning mode.
 func (this *Roomba) Clean() error {
 	return this.WriteByte(OpCodes["Clean"])
 }
 
+// TODO: Max command.
+
+// Spot command starts the Spot cleaning mode.
 func (this *Roomba) Spot() error {
 	return this.WriteByte(OpCodes["Spot"])
 }
 
+// SeekDock command sends Roomba to the dock.
 func (this *Roomba) SeekDock() error {
 	return this.WriteByte(OpCodes["SeekDock"])
 }
 
+// TODO: Schedule, Set Day/Time.
+
+// Power command powers down Roomba.
 func (this *Roomba) Power() error {
 	return this.WriteByte(OpCodes["Power"])
 }
 
+// Drive command controls Roomba’s drive wheels. It takes two 16-bit signed
+// values. The first one specifies the average velocity of the drive wheels in
+// millimeters per second (mm/s).  The next one specifies the radius in
+// millimeters at which Roomba will turn. The longer radii make Roomba drive
+// straighter, while the shorter radii make Roomba turn more. The radius is
+// measured from the center of the turning circle to the center of Roomba. A
+// Drive command with a positive velocity and a positive radius makes Roomba
+// drive forward while turning toward the left. A negative radius makes Roomba
+// turn toward the right. Special cases for the radius make Roomba turn in place
+// or drive straight. A negative velocity makes Roomba drive backward. Velocity
+// is in range (-500 – 500 mm/s), radius (-2000 – 2000 mm). Special cases:
+// straight = 32768 or 32767 = hex 8000 or 7FFF, turn in place clockwise = -1,
+// turn in place counter-clockwise = 1
 func (this *Roomba) Drive(velocity, radius int16) error {
 	if !(-500 <= velocity && velocity <= 500) {
 		return fmt.Errorf("invalid velocity: %d", velocity)
@@ -79,10 +131,18 @@ func (this *Roomba) Drive(velocity, radius int16) error {
 	return this.Write(OpCodes["Drive"], Pack([]interface{}{velocity, radius}))
 }
 
+// Stop commands is equivalent to Drive(0, 0).
 func (this *Roomba) Stop() error {
 	return this.Drive(0, 0)
 }
 
+// DirectDrive command lets you control the forward and backward motion of
+// Roomba’s drive wheels independently. It takes two 16-bit signed values.
+// The first specifies the velocity of the right wheel in millimeters per second
+// (mm/s), The next one specifies the velocity of the left wheel A positive
+// velocity makes that wheel drive forward, while a negative velocity makes it
+// drive backward. Right wheel velocity (-500 – 500 mm/s). Left wheel velocity
+// (-500 – 500 mm/s).
 func (this *Roomba) DirectDrive(right, left int16) error {
 	if !(-500 <= right && right <= 500) ||
 		!(-500 <= left && left <= 500) {
@@ -91,6 +151,13 @@ func (this *Roomba) DirectDrive(right, left int16) error {
 	return this.Write(OpCodes["DirectDrive"], Pack([]interface{}{right, left}))
 }
 
+// TODO: Drive PWM, Motors, PWM Motors commands.
+
+// LEDs command controls the LEDs common to all models of Roomba 500. The
+// Clean/Power LED is specified by two data bytes: one for the color and the
+// other for the intensity. Color: 0 = green, 255 = red. Intermediate values are
+// intermediate colors (orange, yellow, etc). Intensitiy: 0 = off, 255 = full
+// intensity. Intermediate values are intermediate intensities.
 func (this *Roomba) LEDs(check_robot, dock, spot, debris bool, power_color, power_intensity byte) error {
 	var led_bits byte
 
@@ -102,6 +169,11 @@ func (this *Roomba) LEDs(check_robot, dock, spot, debris bool, power_color, powe
 		led_bits, power_color, power_intensity}))
 }
 
+// TODO: Scheduling LEDs, Digit LEDs ASCII, Buttons, Song, Play.
+
+// Sensors command requests the OI to send a packet of sensor data bytes. There
+// are 58 different sensor data packets. Each provides a value of a specific
+// sensor or group of sensors.
 func (this *Roomba) Sensors(packet_id byte) ([]byte, error) {
 	this.Write(OpCodes["Sensors"], []byte{packet_id})
 	bytes_to_read := constants.SENSOR_PACKET_LENGTH[packet_id]
@@ -120,6 +192,9 @@ func (this *Roomba) Sensors(packet_id byte) ([]byte, error) {
 	return result, nil
 }
 
+// QueryList command lets you ask for a list of sensor packets. The result is
+// returned once, as in the Sensors command. The robot returns the packets in
+/// the order you specify.
 func (this *Roomba) QueryList(packet_ids []byte) ([][]byte, error) {
 	b := new(bytes.Buffer)
 	b.WriteByte(byte(len(packet_ids)))
@@ -145,13 +220,14 @@ func (this *Roomba) QueryList(packet_ids []byte) ([][]byte, error) {
 	return result, nil
 }
 
+// PauseStream command lets you stop steam without clearing the list of
+// requested packets.
 func (this *Roomba) PauseStream() {
 	this.StreamPaused <- true
 }
 
 func (this *Roomba) ReadStream(packet_ids []byte, out chan<- [][]byte) {
-
-	var data_length byte = 0
+	var data_length byte
 	for _, packet_id := range packet_ids {
 		data_length += constants.SENSOR_PACKET_LENGTH[packet_id]
 	}
@@ -235,6 +311,11 @@ func (this *Roomba) ReadStream(packet_ids []byte, out chan<- [][]byte) {
 	}
 }
 
+// Stream command starts a stream of data packets. The list of packets
+// requested is sent every 15 ms, which is the rate Roomba uses to update data.
+// This method of requesting sensor data is best if you are controlling Roomba
+// over a wireless network (which has poor real-time characteristics) with
+// software running on a desktop computer.
 func (this *Roomba) Stream(packet_ids []byte) (<-chan [][]byte, error) {
 	b := new(bytes.Buffer)
 	b.WriteByte(byte(len(packet_ids)))
--- constants/constants.go
+++ constants/constants.go
@@ -44,41 +44,157 @@ var OpCodes = map[string]byte{
 
 // SENSOR_* constants define the packet IDs for declared sensor packets.
 const (
+	// The state of the bumper (0 = no bump, 1 = bump) and wheel drop sensors
+	// (0 = wheel raised, 1 = wheel dropped) are sent as individual bits.
 	SENSOR_BUMP_WHEELS_DROPS = 7
-	SENSOR_WALL              = 8
-	SENSOR_CLIFF_LEFT        = 9
-	SENSOR_CLIFF_FRONT_LEFT  = 10
+
+	// The state of the wall sensor is sent as a 1 bit value (0 = no wall,
+	// 1 = wall seen).
+	SENSOR_WALL = 8
+
+	// The state of the cliff sensor on the left side of Roomba is sent as a 1
+	// bit value (0 = no cliff, 1 = cliff).
+	SENSOR_CLIFF_LEFT = 9
+
+	// The state of the cliff sensor on the front left of Roomba is sent as a 1
+	// bit value (0 = no cliff, 1 = cliff).
+	SENSOR_CLIFF_FRONT_LEFT = 10
+
+	// The state of the cliff sensor on the front right of Roomba is sent as a 1
+	// bit value (0 = no cliff, 1 = cliff).
 	SENSOR_CLIFF_FRONT_RIGHT = 11
-	SENSOR_CLIFF_RIGHT       = 12
-	SENSOR_VIRTUAL_WALL      = 13
+
+	// The state of the cliff sensor on the right side of Roomba is sent as a 1
+	// bit value (0 = no cliff, 1 = cliff).
+	SENSOR_CLIFF_RIGHT = 12
+
+	// The state of the virtual wall detector is sent as a 1 bit value
+	// (0 = no virtual wall detected, 1 = virtual wall detected).
+	SENSOR_VIRTUAL_WALL = 13
+
+	// The state of the four wheel overcurrent sensors are sent as individual
+	// bits (0 = no overcurrent, 1 = overcurrent). There is no overcurrent
+	// sensor for the vacuum on Roomba 500.
 	SENSOR_WHEEL_OVERCURRENT = 14
-	SENSOR_DIRT_DETECT       = 15
+
+	// The level of the dirt detect sensor (0-255).
+	SENSOR_DIRT_DETECT = 15
+
 	//unused = 16
-	SENSOR_IR_OMNI                  = 17
-	SENSOR_IR_LEFT                  = 52
-	SENSOR_IR_RIGHT                 = 53
-	SENSOR_BUTTONS                  = 18
-	SENSOR_DISTANCE                 = 19
-	SENSOR_ANGLE                    = 20
-	SENSOR_CHARGING                 = 21
-	SENSOR_VOLTAGE                  = 22
-	SENSOR_CURRENT                  = 23
-	SENSOR_TEMPERATURE              = 24
-	SENSOR_BATTERY_CHARGE           = 25
-	SENSOR_BATTERY_CAPACITY         = 26
-	SENSOR_WALL_SIGNAL              = 27
-	SENSOR_CLIFF_LEFT_SIGNAL        = 28
-	SENSOR_CLIFF_FRONT_LEFT_SIGNAL  = 29
+
+	// This value identifies the 8-bit IR character currently being received by
+	// Roomba’s omnidirectional receiver.  A value of 0 indicates that no
+	// character is being received. These characters include those sent by the
+	// Roomba Remote, the Dock, Roomba 500 Virtual Walls, Create robots using
+	// the Send-IR command, and user-created devices.
+	SENSOR_IR_OMNI = 17
+
+	// This value identifies the 8-bit IR character currently being received
+	// by Roomba’s left receiver. A value of 0 indicates that no character is
+	// being received. These characters include those sent by the Roomba Remote,
+	// the Dock, Roomba 500 Virtual Walls, Create robots using the Send-IR
+	// command, and user-created devices.
+	SENSOR_IR_LEFT = 52
+
+	// Same as above for right IR receiver.
+	SENSOR_IR_RIGHT = 53
+
+	// The state of the Roomba buttons are sent as individual bits (0 = button
+	// not pressed, 1 = button pressed). The day, hour, minute, clock, and
+	// scheduling buttons that exist only on Roomba 560 and 570 will always
+	// return 0 on a robot without these buttons.
+	SENSOR_BUTTONS = 18
+
+	// The distance that Roomba has traveled in millimeters since the distance
+	// it was last requested is sent as a signed 16-bit value, high byte first.
+	// This is the same as the sum of the distance traveled by both wheels
+	// divided by two. Positive values indicate travel in the forward direction;
+	// negative values indicate travel in the reverse direction. If the value is
+	// not polled frequently enough, it is capped at its minimum or maximum.
+	// Range: -32768 – 32767
+	SENSOR_DISTANCE = 19
+
+	// The angle in degrees that Roomba has turned since the angle was last
+	// requested. Counter-clockwise angles are positive and clockwise angles
+	// are negative. If the value is not polled frequently enough, it is capped
+	// at its minimum or maximum. Range: -32768 – 32767
+	SENSOR_ANGLE = 20
+
+	// This code indicates Roomba’s current charging state. Range: 0 – 5
+	//
+	//  Code Charging State
+	//  0 Not charging
+	//  1 Reconditioning Charging
+	//  2 Full Charging
+	//  3 Trickle Charging
+	//  4 Waiting
+	//  5 Charging Fault Condition
+	SENSOR_CHARGING = 21
+
+	// This code indicates the voltage of Roomba’s battery in millivolts (mV).
+	// Range: 0 – 65535 mV
+	SENSOR_VOLTAGE = 22
+
+	// The current in milliamps (mA) flowing into or out of Roomba’s battery.
+	// Negative currents indicate that the current is flowing out of the
+	// battery, as during normal running. Positive currents indicate that the
+	// current is flowing into the battery, as during charging.
+	// Range: -32768 – 32767 mA
+	SENSOR_CURRENT = 23
+
+	// The temperature of Roomba’s battery in degrees Celsius. Range: -128 – 127
+	SENSOR_TEMPERATURE = 24
+
+	// The current charge of Roomba’s battery in milliamp-hours (mAh). The
+	// charge value decreases as the battery is depleted during running and
+	// increases when the battery is charged. Range: 0 – 65535 mAh
+	SENSOR_BATTERY_CHARGE = 25
+
+	// The estimated charge capacity of Roomba’s battery in milliamp-hours (mAh). Range: 0 – 65535 mAh
+	SENSOR_BATTERY_CAPACITY = 26
+
+	// The strength of the wall signal is returned as an unsigned 16-bit value.
+	// Range: 0-1023.
+	SENSOR_WALL_SIGNAL = 27
+
+	// The strength of the cliff left signal. Range: 0-4095.
+	SENSOR_CLIFF_LEFT_SIGNAL = 28
+
+	// The strength of the cliff front left signal. Range: 0-4095.
+	SENSOR_CLIFF_FRONT_LEFT_SIGNAL = 29
+
+	// The strength of the cliff front right signal. Range: 0-4095
 	SENSOR_CLIFF_FRONT_RIGHT_SIGNAL = 30
-	SENSOR_CLIFF_RIGHT_SIGNAL       = 31
+
+	// The strength of the cliff right signal. Range: 0-4095
+	SENSOR_CLIFF_RIGHT_SIGNAL = 31
 	//unused = 32-33
-	SENSOR_CHARGING_SOURCE    = 34
-	SENSOR_OI_MODE            = 35
-	SENSOR_SONG_NUMBER        = 36
-	SENSOR_SONG_PLAYING       = 37
+
+	// Roomba’s connection to the Home Base and Internal Charger are returned as individual bits.
+	SENSOR_CHARGING_SOURCE = 34
+
+	// The current OI mode is returned. Range 0-3.
+	//  Number Mode
+	//  0 Off
+	//  1 Passive
+	//  2 Safe
+	//  3 Full
+	SENSOR_OI_MODE = 35
+
+	// The currently selected OI song is returned. Range: 0-15
+	SENSOR_SONG_NUMBER = 36
+
+	// The state of the OI song player is returned. 1 = OI song currently playing; 0 = OI song not playing.
+	SENSOR_SONG_PLAYING = 37
+
+	// The number of data stream packets is returned.
 	SENSOR_NUM_STREAM_PACKETS = 38
+
+	// The velocity most recently requested with a Drive command.
 	SENSOR_REQUESTED_VELOCITY = 39
-	SENSOR_REQUESTED_RADIUS   = 40
+
+	// The radius most recently requested with a Drive command.
+	SENSOR_REQUESTED_RADIUS = 40
 	//....
 	SENSOR_ALL = 100
 )
